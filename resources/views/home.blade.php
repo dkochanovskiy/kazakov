b907937293e9b3ff4b192fa6a4d3d66803a77a73
Денис <dkochanovskiy@gmail.com>
2018-07-11 13:02:54 +0300

    виды товаров подсветка при наведении

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,55 +1,47 @@
 @extends('template.template')
 @section('content')
     <div class="col-md-12">
-        <div class="col-md-3">
-            <wrapper>
-                <div id="dws-slider" class="carousel slide" data-ride="carousel">
+        <div class="col-md-3" style="">
+            <div class="wrapper">
+                <div id="drawingsWithSand" class="carousel slide" data-ride="carousel">
                     <!--Показатели-->
                     <ol class="carousel-indicators">
-                        <li data-target="#dws-slider" data-slide-to="0" class="active"></li>
-                        <li data-target="#dws-slider" data-slide-to="1"></li>
-                        <li data-target="#dws-slider" data-slide-to="2"></li>
+                        <li data-target="#drawingsWithSand" data-slide-to="0" class="active"></li>
+                        <li data-target="#drawingsWithSand" data-slide-to="1"></li>
+                        <li data-target="#drawingsWithSand" data-slide-to="2"></li>
                     </ol>
 
                     <!--Обертка для слайдов-->
                     <div class="carousel-inner" role="listbox">
-                        <div class="item active"><img src="img/slider-1.jpg" alt="Картинка 1">
-                            <div class="carousel-caption">
-                                <h3 class="text-uppercase">Адаптивный слайдер</h3>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin aliquet elit lorem, ac congue mi
-                                    eleifend sit amet. Sed dignissim viverra neque a tristique.</p>
-                            </div>
+                        <div class="item active">
+                            <img src="{{ asset('images/slider-1.jpg') }}" alt="Рисунок песком">
                         </div>
-                        <div class="item"><img src="img/slider-2.jpg" alt="Картинка 2">
-                            <div class="carousel-caption">
-                                <h3 class="text-uppercase">Анимированная прокрутка</h3>
-                                <p>Aenean cursus imperdiet erat sit amet facilisis. Phasellus congue, sem in consectetur accumsan,
-                                    tellus risus sollicitudin mauris, quis ornare libero magna eget ex.</p>
-                            </div>
+                        <div class="item">
+                            <img src="{{ asset('images/slider-1.jpg') }}" alt="Рисунок песком">
                         </div>
-                        <div class="item"><img src="img/slider-3.jpg" alt="Картинка 3">
-                            <div class="carousel-caption">
-                                <h3 class="text-uppercase">Простая установка</h3>
-                                <p>Praesent dictum, orci eget eleifend auctor, urna ex dapibus odio, vitae pretium neque massa vel
-                                    neque. Donec et interdum diam. Morbi dignissim vestibulum mi ac viverra.</p>
-                            </div>
+                        <div class="item">
+                            <img src="{{ asset('images/slider-1.jpg') }}" alt="Рисунок песком">
                         </div>
                     </div>
 
                     <!--Элементы управления-->
-                    <a class="left carousel-control" href="#dws-slider" role="button" data-slide="prev">
+                    <a class="left carousel-control" href="#drawingsWithSand" role="button" data-slide="prev">
                         <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
                         <span class="sr-only">Previous</span>
                     </a>
-                    <a class="right carousel-control" href="#dws-slider" role="button" data-slide="next">
+                    <a class="right carousel-control" href="#drawingsWithSand" role="button" data-slide="next">
                         <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
                         <span class="sr-only">Next</span>
                     </a>
                 </div>
-            </wrapper>
+            </div>
         </div>
         <div class="col-md-9" style="font-size: 18px">
-            Задача организации, в особенности же начало повседневной работы по формированию позиции играет важную роль в формировании позиций, занимаемых участниками в отношении поставленных задач. Идейные соображения высшего порядка, а также реализация намеченных плановых заданий требуют определения и уточнения позиций, занимаемых участниками в отношении поставленных задач. С другой стороны консультация с широким активом способствует подготовки и реализации систем массового участия.
+            <p>Задача организации, в особенности же начало повседневной работы по формированию позиции играет важную
+                роль в формировании позиций, занимаемых участниками в отношении поставленных задач. Идейные соображения
+                высшего порядка, а также реализация намеченных плановых заданий требуют определения и уточнения позиций,
+                занимаемых участниками в отношении поставленных задач. С другой стороны консультация с широким активом
+                способствует подготовки и реализации систем массового участия.</p>
         </div>
     </div>
     <hr class="col-md-12">
@@ -61,14 +53,16 @@
                         <div class="row">
                             <div class="col-md-12" style="">
                                 <div class="row">
-                                    <div class="col-md-offset-4 col-md-1" style="font-size: 22px;">
-                                        Столы
+                                    <div class="col-md-offset-5 col-md-1" style="font-size: 22px;">
+                                        <h3 class="center-block">
+                                            Столы
+                                        </h3>
                                     </div>
                                 </div>
                                 <div class="row">
                                     <a href="{{ route('tables') }}">
-                                        <div class="col-md-offset-1 col-md-10" style="outline: 1px solid #c0c0c0; height:100px;">
-
+                                        <div class="col-md-offset-1 col-md-10 type-of-goods">
+                                            <img class="center-block" src="{{ asset('images/table.jpg') }}" alt="Стол для рисования песком">
                                         </div>
                                     </a>
                                 </div>
@@ -80,13 +74,15 @@
                             <div class="col-md-12" style="">
                                 <div class="row">
                                     <div class="col-md-offset-4 col-md-1" style="font-size: 22px;">
-                                        Аксессуары
+                                        <h3 class="center-block">
+                                            Аксессуары
+                                        </h3>
                                     </div>
                                 </div>
                                 <div class="row">
                                     <a href="{{ route('accessories') }}">
-                                        <div class="col-md-offset-1 col-md-10" style="outline: 1px solid #c0c0c0; height:100px;">
-
+                                        <div class="col-md-offset-1 col-md-10 type-of-goods">
+                                            <img class="center-block" src="{{ asset('images/accessories.jpg') }}" alt="Аксессуары для рисования песком">
                                         </div>
                                     </a>
                                 </div>
@@ -98,7 +94,9 @@
                             <div class="col-md-12" style="/*height:537px;*/ margin-top: 30px;">
                                 <div class="row">
                                     <div class="col-md-offset-5 col-md-1" style="font-size: 22px;">
-                                        Отзывы
+                                        <h3>
+                                            Отзывы
+                                        </h3>
                                     </div>
                                 </div>
                                 <form method="post" class="col-md-12 form-horizontal" role="form">
@@ -157,7 +155,9 @@
                             <div class="col-md-offset-1 col-md-11" style="">
                                 <div class="row">
                                     <div class="col-md-offset-3 col-md-1" style="font-size: 22px;">
-                                        Статьи
+                                        <h3>
+                                            Статьи
+                                        </h3>
                                     </div>
                                 </div>
                                 <div class="col-md-12" style="height:653px; border: 1px solid #c0c0c0; border-radius: 5px; overflow:auto;">
@@ -165,8 +165,23 @@
                                         <div class="col-md-12" style="color: #449d44; font-size: 18px; padding: 10px 0 10px 15px;">
                                             Рисунки песком
                                         </div>
-                                        <div class="col-md-12" style=" height: 30px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">
-                                            С другой стороны дальнейшее развитие различных форм деятельности играет важную роль в формировании существенных финансовых и административных условий. Задача организации, в особенности же укрепление и развитие структуры позволяет выполнять важные задания по разработке системы обучения кадров, соответствует насущным потребностям. Разнообразный и богатый опыт начало повседневной работы по формированию позиции играет важную роль в формировании форм развития. Идейные соображения высшего порядка, а также реализация намеченных плановых заданий позволяет оценить значение новых предложений. Повседневная практика показывает, что рамки и место обучения кадров в значительной степени обуславливает создание существенных финансовых и административных условий. Задача организации, в особенности же дальнейшее развитие различных форм деятельности представляет собой интересный эксперимент проверки соответствующий условий активизации.
+                                        <div class="col-md-12" style="width: 300px; height: 30px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">
+                                            <p style="column-width: 150px; height: 100%;">
+                                                С другой стороны дальнейшее развитие различных форм деятельности играет
+                                                важную роль в формировании существенных финансовых и административных
+                                                условий. Задача организации, в особенности же укрепление и развитие
+                                                структуры позволяет выполнять важные задания по разработке системы
+                                                обучения кадров, соответствует насущным потребностям. Разнообразный и
+                                                богатый опыт начало повседневной работы по формированию позиции играет
+                                                важную роль в формировании форм развития. Идейные соображения высшего
+                                                порядка, а также реализация намеченных плановых заданий позволяет
+                                                оценить значение новых предложений. Повседневная практика показывает,
+                                                что рамки и место обучения кадров в значительной степени обуславливает
+                                                создание существенных финансовых и административных условий. Задача
+                                                организации, в особенности же дальнейшее развитие различных форм
+                                                деятельности представляет собой интересный эксперимент проверки
+                                                соответствующий условий активизации.
+                                            </p>
                                         </div>
                                         <div class="col-md-12">
                                             <a href="">Читать полностью</a>
@@ -176,7 +191,16 @@
                                             Психологические песочницы
                                         </div>
                                         <div class="col-md-12" style="height: 30px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">
-                                            Равным образом новая модель организационной деятельности способствует подготовки и реализации дальнейших направлений развития. Разнообразный и богатый опыт укрепление и развитие структуры обеспечивает широкому кругу (специалистов) участие в формировании системы обучения кадров, соответствует насущным потребностям. Не следует, однако забывать, что рамки и место обучения кадров способствует подготовки и реализации позиций, занимаемых участниками в отношении поставленных задач. С другой стороны начало повседневной работы по формированию позиции в значительной степени обуславливает создание соответствующий условий активизации.
+                                            <p>Равным образом новая модель организационной деятельности способствует
+                                                подготовки и реализации дальнейших направлений развития. Разнообразный и
+                                                богатый опыт укрепление и развитие структуры обеспечивает широкому кругу
+                                                (специалистов) участие в формировании системы обучения кадров,
+                                                соответствует насущным потребностям. Не следует, однако забывать, что
+                                                рамки и место обучения кадров способствует подготовки и реализации
+                                                позиций, занимаемых участниками в отношении поставленных задач. С другой
+                                                стороны начало повседневной работы по формированию позиции в
+                                                значительной степени обуславливает создание соответствующий условий
+                                                активизации.</p>
                                         </div>
                                         <div class="col-md-12">
                                             <a href="">Читать полностью</a>
@@ -186,7 +210,18 @@
                                             Дети в восторге
                                         </div>
                                         <div class="col-md-12" style="height: 30px; white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">
-                                            Товарищи! консультация с широким активом в значительной степени обуславливает создание новых предложений. Разнообразный и богатый опыт начало повседневной работы по формированию позиции играет важную роль в формировании направлений прогрессивного развития. Товарищи! постоянный количественный рост и сфера нашей активности играет важную роль в формировании новых предложений. Задача организации, в особенности же реализация намеченных плановых заданий играет важную роль в формировании соответствующий условий активизации. Разнообразный и богатый опыт дальнейшее развитие различных форм деятельности позволяет выполнять важные задания по разработке дальнейших направлений развития. Товарищи! рамки и место обучения кадров обеспечивает широкому кругу (специалистов) участие в формировании новых предложений.
+                                            <p>Товарищи! консультация с широким активом в значительной степени
+                                                обуславливает создание новых предложений. Разнообразный и богатый опыт
+                                                начало повседневной работы по формированию позиции играет важную роль в
+                                                формировании направлений прогрессивного развития. Товарищи! постоянный
+                                                количественный рост и сфера нашей активности играет важную роль в
+                                                формировании новых предложений. Задача организации, в особенности же
+                                                реализация намеченных плановых заданий играет важную роль в формировании
+                                                соответствующий условий активизации. Разнообразный и богатый опыт
+                                                дальнейшее развитие различных форм деятельности позволяет выполнять
+                                                важные задания по разработке дальнейших направлений развития. Товарищи!
+                                                рамки и место обучения кадров обеспечивает широкому кругу (специалистов)
+                                                участие в формировании новых предложений.</p>
                                         </div>
                                         <div class="col-md-12">
                                             <a href="">Читать полностью</a>
--- resources/views/template/template.blade.php
+++ resources/views/template/template.blade.php
@@ -4,9 +4,16 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/slider/style.css') }}" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/slider/media.css') }}" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap/bootstrap.css') }}" />
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap/bootstrap.min.css') }}" />
     <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+
     <script src="//netdna.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <link href="https://fonts.googleapis.com/css?family=Noto+Sans" rel="stylesheet">
     <title>kazakov</title>
 </head>
@@ -66,12 +73,11 @@
             </div>
         </div>
     </footer>
-
-        <script src="{{ asset('js/jquery-1.7.2.min.js') }}"></script>
-        <script src="{{ asset('js/jquery.maskedinput.min.js') }}"></script>
-        <script src="{{ asset('js/script.js') }}"></script>
-
-        <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
-        <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+    <script src="{{ asset('js/jquery-1.7.2.min.js') }}"></script>
+    <script src="{{ asset('js/jquery.maskedinput.min.js') }}"></script>
+    <script src="{{ asset('js/script.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.js') }}"></script>
+    <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+    <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
     </body>
 </html>
